c52c70b0c752ad1446988beff83cc53f640db3e0
kanaks2010 <kanaks2010@gmail.com>
2015-03-31 15:00:44 +0600

    updated page routing

--- playframework/app/controllers/Application.java
+++ playframework/app/controllers/Application.java
@@ -11,4 +11,15 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready.You can change your app within a minute"));
     }
 
+    
+    public static Result pprofile() {
+        return ok(patientprofile2.render("Your new application is ready.You can change your app within a minute"));
+    }
+    
+    public static Result dprofile() {
+        return ok(doctorprofile2.render("Your new application is ready.You can change your app within a minute"));
+    }
+    public static Result main() {
+        return ok(index.render("Your new application is ready.You can change your app within a minute"));
+    }
 }
