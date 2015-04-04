80ee78911e53c4d9abcae09c99717dc080c5e1c6
kanaks2010 <kanaks2010@gmail.com>
2015-04-04 16:02:57 +0600

    Create profile page complete

--- playframework/app/controllers/Application.java
+++ playframework/app/controllers/Application.java
@@ -8,16 +8,16 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready.You can change your app within a minute"));
+        return ok(index.render(""));
     }
-
-    
     public static Result pprofile() {
-        return ok(patientprofile2.render("Your new application is ready.You can change your app within a minute"));
+        return ok(patientprofile2.render(""));
     }
-    
     public static Result dprofile() {
-        return ok(doctorprofile2.render("Your new application is ready.You can change your app within a minute"));
+        return ok(doctorprofile2.render(""));
+    }
+    public static Result crprofile() {
+		return ok(createprofile2.render(""));
     }
     public static Result main() {
         return ok(index.render("Your new application is ready.You can change your app within a minute"));
